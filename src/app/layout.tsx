84bb94049de24a4b305adce25d9e2026bbe8e84a
Gabriel Ochube <sidochube@gmail.com>
2026-02-09 03:49:14 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={cn('font-body antialiased')}>
+      <body className={cn('font-body antialiased')} suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster />
