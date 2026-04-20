7e446dd6873f2671db464e954d2d1293334619b4
Gabriel Ochube <sidochube@gmail.com>
2026-04-20 04:09:29 +0000

    scale iamge down to 1x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -199,7 +199,7 @@ export default function AthenaLandingPage() {
                   src="/images/athena-classroom.png"
                   alt="Athena classroom"
                   fill
-                  className="object-cover scale-[2]"
+                  className="object-cover"
                   priority
                 />
               </div>
